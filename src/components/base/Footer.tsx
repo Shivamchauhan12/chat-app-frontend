0986be0a583cacab85d02704f68794882e93d6df
Shivamchauhan12 <shivamchauhan57332@gmail.com>
2026-01-18 23:45:36 +0530

    updated the footer

--- src/components/base/Footer.tsx
+++ src/components/base/Footer.tsx
@@ -8,18 +8,7 @@ export default function Footer() {
     <footer className="p-6 bg-gray-900 text-white">
       <div className="flex justify-between">
         <div>
-          <div>© 2024 QuickChat. All rights reserved.</div>
-          <div className="space-x-4 mt-2">
-            <Link href="/privacy-policy">Privacy Policy</Link>
-            <Link href="/terms-of-service">Terms of Service</Link>
-          </div>
-        </div>
-        <div className="space-y-4">
-          <Input
-            placeholder="Subscribe to our newsletter"
-            className="bg-gray-800 border-none"
-          />
-          <Button>Subscribe</Button>
+          <div>© 2026 QuickChat. All rights reserved.</div>
         </div>
       </div>
     </footer>
